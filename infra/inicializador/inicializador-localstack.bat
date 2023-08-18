af08d92a1fea426e7b71189509775a1bbe872c79
Carlos Roberto <carlosmedeiroslima@gmail.com>
2023-08-17 21:35:20 -0300

    Atualizando o Bat para configuração do ambiente

--- infra/inicializador/inicializador-localstack.bat
+++ infra/inicializador/inicializador-localstack.bat
@@ -5,8 +5,23 @@ echo ### Criando Chaves no AWS Parameter Store do LocalStack...
 aws --endpoint http://localhost:4566 --profile dev ssm put-parameter --name "/config/my-demo-boot_dev/helloworld" --value "Hello World Parameter Store" --type String
 
 
+echo ### Criando SQS no LocalStack...
+aws --endpoint http://localhost:4566 --profile dev sqs create-queue --queue-name sqs-consulta-cliente
+aws --endpoint http://localhost:4566 --profile dev sqs create-queue --queue-name sqs-retorno-consulta-cliente
+aws --endpoint http://localhost:4566 --profile dev sqs create-queue --queue-name sqs-inclusao-cliente-assync
+aws --endpoint http://localhost:4566 --profile dev sqs create-queue --queue-name sqs-reenvio-consulta-cliente
 
 
+echo ### Criando um Topico SNS no LocalStack...
+aws --endpoint-url=http://localhost:4566 --profile dev sns create-topic --name sns-disponibiliza-dados-aplicacao-cliente 
 
-echo ### Criando Queue(Standard) no SQS do LocalStack...
-aws --endpoint http://localhost:4566 sqs create-queue --queue-name sqsHelloWorld
+# Cria uma assinatura para o Topico
+aws --endpoint-url=http://localhost:4566 --profile dev sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:sns-disponibiliza-dados-aplicacao-cliente --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:FILA_DYNAMO 
+
+#Criacao da tabela do DynamoDB
+# Cria uma tabela no dynamodb com nome TabelaMensagens
+aws --endpoint-url=http://localhost:4566 --profile dev dynamodb create-table --table-name dynamo-cliente-db --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 
+
+echo ### Banco de Dados Rds a Criar
+
+
