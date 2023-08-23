46f39c8bbbe0694823e83cee85e2a7727f550604
Carlos Roberto <carlosmedeiroslima@gmail.com>
2023-08-22 21:43:58 -0300

    Atualizando o sistema

--- stop-container.sh
+++ stop-container.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 #docker-compose -f authorization_server.yml down
-docker-compose -f C:/Desenvolvimento/sistemas/study-localstack-aws/infra/stack.yml down
+docker-compose -f C:/Desenvolvimento/sistemas/study-cliente-flow/infra/stack.yml down
 
 echo 'Stack finished !'
